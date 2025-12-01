59c80714a035984a9c5122e954f6fabbda81f859
pupungeggang2 <kyongkyong2892@gmail.com>
2025-12-02 01:21:31 +0900

    temp

--- src/code/game.hpp
+++ src/code/game.hpp
@@ -9,7 +9,7 @@ class Game : public std::enable_shared_from_this<Game> {
     public:
         shared_ptr<Game> self;
         shared_ptr<Scene> scene;
-        sf::RenderWindow window; unsigned int width, height; bool running = true;
+        sf::RenderWindow window; unsigned int width, height; bool running = true;   
         sf::View viewUI;
         sf::RectangleShape rRect; sf::Font rFont; sf::Text rText = sf::Text(rFont, "", 32);
         float frameCurrent = 0, framePrevious = 0, delta = 0; sf::Clock clock;
